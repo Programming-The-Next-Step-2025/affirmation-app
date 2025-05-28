4a022f1d6504fa0d6f691bfd5bff4818d2ed5b06
sonimoni13 <goldchikulya@gmail.com>
2025-05-28 09:39:09 +0200

    definition of the main functions improved

---
+++ affirmations/R/random_affirmation.R
@@ -0,0 +1,20 @@
+#' Get a Random Affirmation from Any Personality
+#'
+#' This function randomly selects and returns one affirmation
+#' from the full set of affirmations across all personality types
+#' in the `personality_data` list.
+#'
+#' @return A character string containing a randomly selected affirmation.
+#' @examples
+#' random_affirmation()
+#' @export
+random_affirmation <- function() {
+  all_affirmations <- unname(unlist(lapply(personality_data, function(x) x$affirmations)))
+  sample(all_affirmations, 1)
+}
+
+
+
+
+
+
